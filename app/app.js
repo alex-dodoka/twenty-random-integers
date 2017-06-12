ba28367b5d3cded4cd6b6cc9a2435f8c8101d711
alex-dodoka <neksus.92@mail.ru>
2017-06-12 17:39:43 +0300

    Adding logic to the code structure

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ let randomNumber = function () {
     return Math.floor(Math.random() * 100);
 };
 
+for (let i = 0; i < 20; i++) {
+    arrOfRandomNumber.push(randomNumber());
+}
+
 function sortForArr(a, b) {
     if (a > b) {
         return 1
@@ -17,9 +21,6 @@ function sortForArr(a, b) {
     }
 }
 
-for (let i = 0; i < 20; i++) {
-    arrOfRandomNumber.push(randomNumber());
-}
 document.write(arrOfRandomNumber);
 document.write(`<br>Самое маленькое число : ${sortedArr[0]}, 
 а самое большое число,это ${sortedArr[sortedArr.length - 1]}`);
